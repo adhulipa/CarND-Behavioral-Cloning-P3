c6aa1d6afc8c9772e172dfea5dc826d6dba4092b
Aditya Dhulipala <dhula@amazon.com>
2017-09-30 18:37:40 -0700

    Trying to define a generator

--- Model.py
+++ Model.py
@@ -2,26 +2,55 @@ import csv
 import cv2
 import numpy as np
 
-from pre_process import read_data
+import pre_process
+from sklearn.model_selection import train_test_split
 
 data_directory = '/home/carnd/projects/P3/DataFromMac/p3-training-data/track1-normal-driving/'
-images, measurements = read_data(data_directory)
+driving_log_file = data_directory + 'driving_log.csv'
+driving_img_folder = data_directory + 'IMG/'
 
-# Define inputs to model
-X_train = np.array(images)
-y_train = np.array(measurements)
+samples = pre_process.init(driving_log_file)
+val_percent
+train_samples, validation_samples = train_test_split(samples, test_size=val_percent)
 
+# X_train, y_train = pre_process.load_batch(samples, driving_img_folder)
+
+training_data = pre_process.load_batch(train_samples, driving_img_folder)
+validation_data = pre_process.load_batch(validation_samples, driving_img_folder)
 
 # Design neural network model
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda
+from keras.layers import *
+
+def resize_function(inp):
+    new_height = 66
+    new_width = 200
+    from keras.backend import tf as ktf
+    return ktf.image.resize_images(inp, (new_height, new_width))
 
 model = Sequential()
-model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
-model.add(Flatten(input_shape=(160,320,3)))
+model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3))) # Normalization layer
+model.add(Cropping2D(cropping=((65, 15), (1, 1)), input_shape=(160, 320, 3)))
+model.add(Lambda(resize_function)) # re-size inside the model
+
+model.add(Convolution2D(24,5,5, input_shape=(66,200,3)))
+# model.add(Convolution2D(36, 5,5))
+# model.add(Convolution2D(48,5,5))
+
+# model.add(Convolution2D(64,3,3))
+# model.add(Convolution2D(64,3,3))
+
+model.add(Flatten())
+
+# model.add(Dense(1164))
+# model.add(Dense(100))
+# model.add(Dense(50))
+# model.add(Dense(10))
 model.add(Dense(1))
+
 model.compile(loss='mse', optimizer='adam')
 
 # Train model
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
+# model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
+model.fit_generator(training_data, len(samples), validation_data=validation_data, nb_val_samples=len(samples)*val_percent, nb_epoch=5)
 model.save('model.h5')
--- pre_process.py
+++ pre_process.py
@@ -1,16 +1,32 @@
 import csv
 import cv2
+import random
+import numpy as np
 
-def read_data(data_directory):
+import sklearn.utils
+
+
+def _with_a_chance_of(prob):
+    return random.random() < prob
+
+def _is_steering_straight(angle):
+    return angle <= 0.85
+
+def _flip_l2r_randomly(image, measurement):
+    if (_with_a_chance_of(0.5)):
+        image = np.fliplr(image)
+        measurement = -measurement
+    return (image, measurement)
+
+def init(driving_log_file):
     '''
     data_directory = '/home/carnd/projects/P3/DataFromMac/p3-training-data/track1-normal-driving/'
     driving_log_file = '/home/carnd/projects/P3/DataFromMac/p3-training-data/track1-normal-driving/driving_log.csv'
     driving_img_folder = '/home/carnd/projects/P3/DataFromMac/p3-training-data/track1-normal-driving/IMG/'
-    read_data(data_directory)
+    init('/home/carnd/projects/P3/DataFromMac/p3-training-data/track1-normal-driving/driving_log.csv')
     '''
-
-    driving_log_file = data_directory + 'driving_log.csv'
-    driving_img_folder = data_directory + 'IMG/'
+    # driving_log_file = data_directory + 'driving_log.csv'
+    # driving_img_folder = data_directory + 'IMG/'
 
     # Read data
     lines = []
@@ -18,14 +34,37 @@ def read_data(data_directory):
         reader = csv.reader(csvfile)
         for line in reader:
             lines.append(line)
+    return lines
 
+def load_batch(samples, driving_img_folder, batch_size=32):
+    '''
+    load_batch(lines, '/home/carnd/projects/P3/DataFromMac/p3-training-data/track1-normal-driving/IMG/')
+    '''
+    while True:
+        num_samples = len(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_lines = samples[offset:offset+batch_size]
+
+        (images, measurements) = _read_images_measurements_batch(batch_lines, driving_img_folder)
+
+        X_train = np.array(images)
+        y_train = np.array(measurements)
+        yield sklearn.utils.shuffle(X_train, y_train)
+
+def _read_images_measurements_batch(batch_lines, driving_img_folder):
     images = []
     measurements = []
-    for line in lines:
+    for line in batch_lines:
+        measurement = float(line[3])
         source_path = line[0]
         filename = source_path.split('/')[-1]
         current_path = driving_img_folder + filename
         image = cv2.imread(current_path)
+
+        # Randomly flip the images
+        image, measurement = _flip_l2r_randomly(image, measurement)
+
         images.append(image)
-        measurements.append(float(line[3]))
+        measurements.append(measurement)
+
     return (images, measurements)
