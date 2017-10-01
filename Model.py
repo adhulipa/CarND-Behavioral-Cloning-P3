0f4a4c0ca0c81b285fcf750c98e4e94a2606d8b1
Aditya Dhulipala <dhula@amazon.com>
2017-09-30 18:40:21 -0700

    Last known working model

--- Model.py
+++ Model.py
@@ -3,20 +3,9 @@ import cv2
 import numpy as np
 
 import pre_process
-from sklearn.model_selection import train_test_split
 
 data_directory = '/home/carnd/projects/P3/DataFromMac/p3-training-data/track1-normal-driving/'
-driving_log_file = data_directory + 'driving_log.csv'
-driving_img_folder = data_directory + 'IMG/'
-
-samples = pre_process.init(driving_log_file)
-val_percent
-train_samples, validation_samples = train_test_split(samples, test_size=val_percent)
-
-# X_train, y_train = pre_process.load_batch(samples, driving_img_folder)
-
-training_data = pre_process.load_batch(train_samples, driving_img_folder)
-validation_data = pre_process.load_batch(validation_samples, driving_img_folder)
+X_train, y_train = pre_process.read_data(data_directory)
 
 # Design neural network model
 from keras.models import Sequential
@@ -51,6 +40,5 @@ model.add(Dense(1))
 model.compile(loss='mse', optimizer='adam')
 
 # Train model
-# model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
-model.fit_generator(training_data, len(samples), validation_data=validation_data, nb_val_samples=len(samples)*val_percent, nb_epoch=5)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
 model.save('model.h5')
--- pre_process.py
+++ pre_process.py
@@ -4,6 +4,7 @@ import random
 import numpy as np
 
 import sklearn.utils
+from sklearn.model_selection import train_test_split
 
 
 def _with_a_chance_of(prob):
@@ -18,15 +19,16 @@ def _flip_l2r_randomly(image, measurement):
         measurement = -measurement
     return (image, measurement)
 
-def init(driving_log_file):
+def read_data(data_directory, batch_size=19283719287312):
     '''
     data_directory = '/home/carnd/projects/P3/DataFromMac/p3-training-data/track1-normal-driving/'
     driving_log_file = '/home/carnd/projects/P3/DataFromMac/p3-training-data/track1-normal-driving/driving_log.csv'
     driving_img_folder = '/home/carnd/projects/P3/DataFromMac/p3-training-data/track1-normal-driving/IMG/'
-    init('/home/carnd/projects/P3/DataFromMac/p3-training-data/track1-normal-driving/driving_log.csv')
+    read_data(data_directory)
     '''
-    # driving_log_file = data_directory + 'driving_log.csv'
-    # driving_img_folder = data_directory + 'IMG/'
+
+    driving_log_file = data_directory + 'driving_log.csv'
+    driving_img_folder = data_directory + 'IMG/'
 
     # Read data
     lines = []
@@ -34,28 +36,29 @@ def init(driving_log_file):
         reader = csv.reader(csvfile)
         for line in reader:
             lines.append(line)
-    return lines
+    train_lines, validation_lines = train_test_split(lines, test_size=0.2)
 
-def load_batch(samples, driving_img_folder, batch_size=32):
-    '''
-    load_batch(lines, '/home/carnd/projects/P3/DataFromMac/p3-training-data/track1-normal-driving/IMG/')
-    '''
-    while True:
-        num_samples = len(samples)
-        for offset in range(0, num_samples, batch_size):
-            batch_lines = samples[offset:offset+batch_size]
+    num_samples = len(lines)
+    for offset in range(0, num_samples, batch_size):
+        batch_lines = lines[offset:offset+batch_size]
 
-        (images, measurements) = _read_images_measurements_batch(batch_lines, driving_img_folder)
+    (images, measurements) = _read_batch(batch_lines, driving_img_folder)
 
-        X_train = np.array(images)
-        y_train = np.array(measurements)
-        yield sklearn.utils.shuffle(X_train, y_train)
+    X_train = np.array(images)
+    y_train = np.array(measurements)
 
-def _read_images_measurements_batch(batch_lines, driving_img_folder):
+    return (X_train, y_train)
+
+def _read_batch(batch_lines, driving_img_folder):
     images = []
     measurements = []
     for line in batch_lines:
         measurement = float(line[3])
+
+        # Exclude like 70% of the data where the car is going straight
+        if (_is_steering_straight(measurement) and _with_a_chance_of(0.7)):
+            continue
+
         source_path = line[0]
         filename = source_path.split('/')[-1]
         current_path = driving_img_folder + filename
